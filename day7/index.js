14cc007b259ee7353e46f0a40cd0f4574de0ae44
adamjayne <adamjayne402@gmail.com>
2021-11-11 10:52:40 -0500

    first attempt at a recursive function, still trying to figure out the accumulator

--- day7/index.js
+++ day7/index.js
@@ -1,42 +1,53 @@
 const parseBag = (buildString) => {
-  console.log(buildString);
   let [bagAttributes, bagContents] = buildString.split(" bags contain ");
-  console.log(bagContents);
   let parsedContents = !bagContents.includes("no other bags")
     ? bagContents.split(", ").map(
         value => ({
           count: parseInt(value[0]),
-          description: value.slice(2).split(" ").slice(0, 2).join("")
+          description: value.slice(2).split(" ").slice(0, 2).join(" ")
         })
       )
     : [];
   return [bagAttributes, parsedContents];
 }
 
-const day7 = () => {
-  const puzzle = require('fs').readFileSync(__dirname + `/puzzle.txt`).toString().split('.\n');
-  const bagRules = {};
-  let outerCount = 0;
+const checkIfContained = (bag, bagList, accumulation=0) => {
 
-  for (const entry of puzzle) {
-    const [bagName, bagContents] = parseBag(entry);
-
-    if (!Object.keys(bagRules).includes(bagName)) {
-      bagRules[bagName] = bagContents;
-    }
-  }
-  // console.log(parseBag(testing));
-
-  for (const bagContents of Object.values(bagRules)) {
-    if (bagContents.length) {
+  for (const [bagName, bagContents] of Object.entries(bagList)) {
+    if (bagName !== bag && bagContents.length) {
       for (const bagValue of bagContents) {
-        if (bagValue.description === "shinygold") {
-          console.log("found")
+        if (bagValue.description === bag) {
+
+        }
+        if (bagValue.description === bag) {
+          console.log("contained")
+          console.log(accumulation)
+          accumulation += checkIfContained(bagName, bagList, 1);
         }
       }
     }
   }
-  console.log(bagRules['shinygold']);
+
+  return accumulation;
+}
+
+const day7 = () => {
+  const puzzle = require('fs').readFileSync(__dirname + `/practice.txt`).toString().split('.\r\n');
+  const bagRules = {};
+  let outerCount = 0;
+  
+  
+  for (const entry of puzzle) {
+    const [bagName, bagContents] = parseBag(entry);
+    if (!Object.keys(bagRules).includes(bagName)) {
+      bagRules[bagName] = bagContents;
+    }
+  }
+1
+  console.log(bagRules)
+
+  console.log(checkIfContained("shiny gold", bagRules));
+  
 
   const part1 = () => {
     return 0;
